790c849198c04c0eb26f684f291ea259db4fd76d
Sunny Rana <sunnyrana.developer@gmail.com>
2020-07-30 20:59:28 +0530

    url changed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -26,27 +26,27 @@
     </div>
     <div id="app">
     </div>
-    <script src="/js/app.js"></script>
+    <script src="{{ URL::asset('js/app.js') }}"></script>
     <!-- include jQuery -->
-    <script src="/js/jquery.min.js"></script>
+    <script src="{{ URL::asset('js/jquery.min.js') }}"></script>
     <!-- bootstrap -->
-    <script src="/js/bootstrap.min.html"></script>
+    <script src="{{ URL::asset('js/bootstrap.min.html') }}"></script>
     <!-- bootstrap -->
-    <script src="/js/owl.carousel.min.js"></script>
+    <script src="{{ URL::asset('js/owl.carousel.min.js') }}"></script>
     <!-- slick slider  -->
-    <script src="/js/slick.js"></script>
+    <script src="{{ URL::asset('js/slick.js') }}"></script>
     <!-- dscountdown  -->
-    <script src="/js/dscountdown.min.js"></script>
+    <script src="{{ URL::asset('js/dscountdown.min.js') }}"></script>
     <!-- jquery.nice-select -->
-    <script src="/js/jquery.nice-select.js"></script>
+    <script src="{{ URL::asset('js/jquery.nice-select.js') }}"></script>
     <!-- magnific-popup -->
-    <script src="/js/jquery.magnific-popup.min.js"></script>
+    <script src="{{ URL::asset('js/jquery.magnific-popup.min.js') }}"></script>
     <!-- Mixitup -->
-    <script src="/js/mixitup.min.js"></script>
+    <script src="{{ URL::asset('js/mixitup.min.js') }}"></script>
     <!-- Google Map -->
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBP1lPhUhDU6MINpruPDinAzXffRlpzzFo"></script>
-    <script src="/js/map.js"></script>
+    <script src="{{ URL::asset('https://maps.googleapis.com/maps/api/js?key=AIzaSyBP1lPhUhDU6MINpruPDinAzXffRlpzzFo') }}"></script>
+    <script src="{{ URL::asset('js/map.js') }}"></script>
     <!-- custom js -->
-    <script src="/js/custom.js"></script>
+    <script src="{{ URL::asset('js/custom.js') }}"></script>
 </body>
 </html>
